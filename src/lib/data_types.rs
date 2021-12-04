6e52d532500b0202a82ea45b3d406e1c10847a4d
Rinrin0413 <qqt45fhd@ab.auone-net.jp>
2021-12-05 05:36:34 +0900

    Add:convertモジュール(std)をインポート&u8とi8の変換及び数字に直で注釈する方法及び整数リテラル,バイトについてを追加

--- src/lib/data_types.rs
+++ src/lib/data_types.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto; // uX を iX にするモジュール
+
 pub fn scala() {
 
 // スカラー型は値(単体の形)です。
@@ -18,4 +20,25 @@ pub fn scala() {
     let i_hachchi:i8 = -128; // -129でとエラー
     let u_hachi:u8 = 255; // 256だとエラー
     println!("i8最小値:{}\nu8最大値{}", i_hachchi, u_hachi);
+
+    // u8 を i8 に変えることもできるが、最大値の違いに注意
+    let u_turnto_i:u8 = 127;
+    let u_turnto_i:i8 = u_turnto_i.try_into().unwrap(); // これでもう符号付なので負の数を扱える
+    println!("{}", -u_turnto_i); // 負の数にしてみる
+
+     // i8 や u8 の注釈は直に数字にでも付けられる。: は不必要
+    let i_hachi_dayo = 127i8;
+    let u_hachi_dayo = 0u8;
+    println!("i8の最大値:{}\nu8の最小値:{}", i_hachi_dayo, u_hachi_dayo);
+
+    //以下は整数リテラル。数字の手前に付けて使う
+    //10進数	        //何もつけないと10進数
+    //16進数	        0x
+    //8進数	            0o
+    //2進数	            0b
+    //バイト(u8-only)   b" "
+    // バイトは違って b"文字" || b"\x<数字>"
+    let binary = 0b1111_0000; // 240の二進数
+    println!("{0:x}", binary); // 16進数にしてみる
+    println!("{:?}", b"\xf0"); // バイトは :? が必要で b"\x<hex>""
 }
