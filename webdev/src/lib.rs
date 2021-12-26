d9b835ff5eca20c4e43d1e3e01069aa60d4e1a10
Rinrin0413 <qqt45fhd@ab.auone-net.jp>
2021-12-26 13:56:32 +0900

    Add:構造体とenumのプライバシについて

--- webdev/src/lib.rs
+++ webdev/src/lib.rs
@@ -35,3 +35,44 @@ fn oji() {}
 mod oya {
     fn ko() { super::oji(); } 
 }
+
+   // 構造体とenumを公開する
+    // 構造体と enum の公開にはいくつかの細かい設定がある
+mod back_of_house {
+    pub struct Breakfast { // メソッドの型枠に構造体を定義
+        pub toast: String, // toastフィールドを公開に
+        seasonal_fruit: String, // フルーツは非公開
+    }
+    impl Breakfast { // 構造体Breakfast の文脈で
+        pub fn summer(toast:&str) -> Breakfast { // summer関数を公開で定義
+            Breakfast { // 初期化省略記法で
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+pub fn eat_at_restaurant() {
+    // 夏にライ麦パン付き朝食を注文
+    let mut meal = back_of_house::Breakfast::summer("Rye"); // back_of_houseモジュールの Breakfastメソッドより summer関数でインスタンスを作成
+    // やっぱり別のパンにする
+    meal.toast = String::from("Wheat"); // 構造体meal の toastフィールドの値を変更
+    println!("I'd like {} toast please", meal.toast);
+    // 食事についてくるフルーツを知ることも修正することも許されていないの以下はエラー
+    //meal.seasonal_fruit = String::from("blueberries");
+}
+
+    // 一方で enum は公開するとその変数はすべて公開される
+    // pub は enumキーワード の前にだけおけばよい
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+pub fn eat_at_restaurant() {
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+    //✔以上で「パス指定やプライバシーの詳細についてI」の参照終了。以降再び ../../src/lib/packages_crates_modules.rs で path_and_privacy節後参照
