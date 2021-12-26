9dc990c9536dd9d32e724b6ff2e421fe7a334718
Rinrin0413 <qqt45fhd@ab.auone-net.jp>
2021-12-26 13:40:22 +0900

    Create:webdev/src/lib.rs

---
+++ webdev/src/lib.rs
@@ -0,0 +1,14 @@
+#![allow(unused)]
+
+mod front_end {
+    pub mod display {
+        pub fn show_icon() {}
+
+        fn show_haeder() {}
+    }
+    pub mod animation {
+        fn rotate_icon() {}
+
+        fn scale_up_header_in_hover() {}
+    }
+}
