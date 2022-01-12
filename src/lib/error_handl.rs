f5f1f1833f772ef73cff3b57d86029dbfe714131
Rinrin0413 <qqt45fhd@ab.auone-net.jp>
2022-01-12 21:41:41 +0900

    Add:?演算子についてii

--- src/lib/error_handl.rs
+++ src/lib/error_handl.rs
@@ -323,4 +323,13 @@ pub fn result() {
 
     // Rust ではこの様なエラー委譲は頻繁に行われるので、
     // これをしやすくする ?演算子が用意されている
+
+   // エラー委譲のショートカット: ?演算子
+    // 以下も同じ機能を read_username_from_file関数の実装だが ?演算子を使用している
+    fn read_username_from_file_ii() -> Result<String, io::Error> {
+        let mut f = File::open("hello.txt")?;
+        let mut s = String::new();
+        f.read_to_string(&mut s)?;
+        Ok(s)
+    }
 }
