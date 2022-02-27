e3a605047be95d082a1fc154430ab56bde64e598
Rinrin0413 <qqt45fhd@ab.auone-net.jp>
2022-02-27 23:48:30 +0900

    Add:関数シグネチャでのライフタイム注釈の例の利用

--- src/lib/lifetime.rs
+++ src/lib/lifetime.rs
@@ -184,4 +184,10 @@ pub fn lifetime() {
 			y
 		}
 	}
+
+    // このコードはコンパイルでき、望んだ動きをするはず
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result); //< The longest string is abcd
 }
