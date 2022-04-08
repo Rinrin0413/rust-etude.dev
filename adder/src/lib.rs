3cdef5eb1ba739e8f76f88a167370563a61c1ed9
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-04-08 18:42:27 +0900

    _

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,1 +1,15 @@
 #![allow(unused)]
+    // 以下は 生成されたモジュール
+
+    /* // 別の場所で被るので無効化
+    // |== BEGIN ==|
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn it_works() {
+            let result = 2 + 2;
+            assert_eq!(result, 4);
+        }
+    }
+    // |== END ==|
+    */
