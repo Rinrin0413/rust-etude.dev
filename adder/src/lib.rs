e38bba7a78826a787d7b2adee2d7db14f9233a31
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-04-09 23:06:33 +0900

    _

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -79,3 +79,8 @@
     // test tests::exploration ... ok
     //
     // test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
+
+    // テスト関数内でパニックが起こるとテストは失敗する
+    // 各テストは新規スレッドで実行され、メインスレッドが テストスレッドが死んだと断定すると、
+    // テストは失敗となる。panic!マクロで試してみる
+    // 新規テスト関数another を見て下さい
