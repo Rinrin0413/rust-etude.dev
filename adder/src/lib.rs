01c7d154f2ffba5bcff82f7455dc4f306670239e
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-04-12 23:28:35 +0900

    _

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -327,3 +327,6 @@
     // test tests_iii::it_adds_two ... ok
     //
     // test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out
+
+    // assert_eq!マクロに与えた第1引数の4と、add_two(2)は等しい
+    // 故にテストは通りました
